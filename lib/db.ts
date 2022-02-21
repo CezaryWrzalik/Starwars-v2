1ae3bf409b4617e37b636ca97eefb2d0bb0b67f2
CezaryWrzalik <WrzalikC@gmail.com>
2022-02-21 15:21:44 +0100

    Amazing debuging

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,6 @@ export const connectToDatabase = async () => {
   const client = await MongoClient.connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aeoqh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
   );
-
-  console.log(client);
+	
   return client;
 };
